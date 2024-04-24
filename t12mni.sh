a0e52071b78a503abefdd981cce38aef26eb84a0
Qiao Kaini <45912337+fujinosai@users.noreply.github.com>
2024-04-24 16:18:01 +0800

    Update t12mni.sh

--- t12mni.sh
+++ t12mni.sh
@@ -1,1 +1,6 @@
+
+## non-linear reg
 antsRegistrationSyN.sh -d 3 -f ${FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz -m ${T1OUTDIR}/masks/t1_brain.nii.gz -o t12mni -n ${NCORE}
+
+## label from t12mni
+antsApplyTransforms -d 3 -r ${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz -i $T1OUTDIR/masks/t1_DKAseg.nii.gz -e 0 -t $T1OUTDIR/reg/t12mni_warp.nii.gz -t $T1OUTDIR/reg/t12mni.mat -o $T1OUTDIR/masks/mni.nii.gz -n GenericLabel
