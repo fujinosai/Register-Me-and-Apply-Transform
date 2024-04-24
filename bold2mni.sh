53c21d7801d23b597147cfa4c737e0e715a17e6a
Qiao Kaini <45912337+fujinosai@users.noreply.github.com>
2024-04-24 16:21:00 +0800

    Create bold2mni.sh

---
+++ bold2mni.sh
@@ -0,0 +1,1 @@
+antsApplyTransforms -e 0 -d 3 -i ${BOLDOUTDIR}/native/rest_reho.nii.gz -o ${BOLDOUTDIR}/mni/rest_reho.nii.gz -n Linear -r ${PhiPipe}/templates/MNI152/MNI152_T1_3mm_brain.nii.gz -t ${T1OUTDIR}/reg/ t12mni1Warp.nii.gz -t ${T1OUTDIR}/reg/t12mni0GenericAffine.mat -t ${BOLDOUTDIR}/reg/rest2t1.mat
